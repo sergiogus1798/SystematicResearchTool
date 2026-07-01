3b97ff1a4830e3e5f1b0304996e0c43a98b76686
sergiogus1798 <sergiogus1998@gmail.com>
2026-07-01 23:34:41 +0200

    Added a few new functions to the main.py to check code

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import Reader
+from src.srt.loader import loader
+from src.srt.overfitting import cscv
 
 stratsDirectory = "NAS100"
 
-ReaderNAS = Reader.Reader(stratsDir=stratsDirectory)
-ReaderNAS.readStrats(returns="Weekly")
+ReaderNAS = loader.loader(stratsDir=stratsDirectory)
+OriginalDataset = ReaderNAS.readStrats(returns="Weekly")
+
+CSCV = cscv.cscv(OriginalDataset, S = 16)
